a569f491c60c96fdb0cb23ec9ee04404440c1a4a
Scott Vaverchak <Scott.Vaverchak@gmail.com>
2019-11-03 12:26:51 -0500

    only allow setting of an argument once, tracking the success of argument parsing rather than managing multiple returns

--- json-gen.py
+++ json-gen.py
@@ -13,25 +13,28 @@ def print_command_line_options():
   return f"{sys.argv[0]}: --options go --here file"
 
 def parse_args(arguments):
+  success = True
   index = 1 
   arguments_out = {}
   while index < len(sys.argv):
     arg = sys.argv[index]
-    
     if arg in arguments:
       arguments_out[arg] = "This is set"
       del(arguments[arg])
+    elif arg in arguments_out:
+      print(f"{arg} is set more than once")
+      success = False
     else:
       print(f"{arg} is an unknown options")
-      return False, arguments_out
+      success = False
 
     index = index + 1
   
   for req_key in (k for k, v in arguments.items() if v):
     print(f"{req_key} is a required argument")
-    return False, arguments_out
+    success = False
 
-  return True, arguments_out 
+  return success, arguments_out 
 
 if __name__ == '__main__':
   main()
